282069b89a46a5808d3676680aaf1eb0e74e9174
Clinton Felix <archimedesfelix1@gmail.com>
2022-04-22 13:05:41 +0100

    Connect To Metamask from Front End UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const {ethers} = require(ethers);
+/*const {ethers} = require(ethers);
 import {ethers} from "ethers";
 
 async function Connect() {
@@ -16,3 +16,40 @@ async function getAccount() {
   showAccount.innerHTML = account;
 }
     }}
+
+    ================================================ */
+
+    const ethereumButton = document.querySelector('.enableEthereumButton');
+    const showAccount = document.querySelector('.showAccount');
+
+    ethereumButton.addEventListener('click', () => {
+    getAccount();
+    });
+    
+
+    async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+    document.querySelector(".enableEthereumButton").innerText = "Connected";
+    }
+    
+
+    import {ethers} from "./ethers.js";
+    console.log(ethers);
+  
+  /*const provider = new ethers.providers.Web3Provider(
+    window.ethereum, "any"
+  );
+
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner(); */
+  export function connect() {
+      if (typeof window.ethereum !== 'undefined') {
+          
+      }
+    /*8let userAddress = await signer.getAddress();
+    document.getElementById("wallet").innerText =
+      "Your wallet is " + userAddress;*/
+  
+  };
